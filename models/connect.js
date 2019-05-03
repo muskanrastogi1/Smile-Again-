6577a7225c573088c39c65096c4455e357d4c1b9
Nikhil Singh <nikhilsingh498@gmail.com>
2019-05-03 11:15:04 +0530

    Db connection added

---
+++ models/connect.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.MONGO_URI)
+
+
+mongoose.connection.once("open", () => {
+    console.log("Sucessfully connected to database")
+}).on("error", (error) => {
+    console.log("There was some error connecting to the database", error)
+})
