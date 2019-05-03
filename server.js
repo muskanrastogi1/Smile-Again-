0977326e90176a7da6b670b5b102ed19fd9510a9
Nikhil Singh <nikhilsingh498@gmail.com>
2019-05-03 11:14:56 +0530

    Initialised server

---
+++ server.js
@@ -0,0 +1,49 @@
+// Installing all the dependencies
+const express = require("express");
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+require("hbs");
+require("dotenv").config();
+
+// connecting to the database
+require("./model/connect.js")
+const middleware = require("./model/middleware.js");
+
+// initialisation 
+const router = express.Router()
+const app = express();
+
+// basic setup 
+app.set('view engine', "hbs");
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+//port 
+const port = process.env.PORT || 3000;
+
+
+//open routes import 
+const openRoutes = require("./routes/openRoutes.js");
+app.use("/", openRoutes);
+
+//auth routes import 
+const authRoutes = require("./routes/auth.js");
+app.use("/auth", authRoutes)
+
+
+//protected routes 
+app.use(middleware.session);
+
+//protected routes (with JSON web token)
+const protectedRoutes = require("./routes/protected.js");
+app.use("/secure", protectedRoutes);
+
+// script to run the server
+app.listen(port, () => {
+    console.log(`Server is up at ${port}`);
+} )
+
+
