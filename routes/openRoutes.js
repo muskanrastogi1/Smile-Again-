4d113cfe01c5b633ffc9f1ed37df1e82f42fd651
Nikhil Singh <nikhilsingh498@gmail.com>
2019-05-03 11:50:20 +0530

    Added open route

---
+++ routes/openRoutes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+
+router.get("/", (request, response) => {
+    response.render("index.hbs");
+})
+
+router.get("/about", (request, response) => {
+    response.send("This is the about route")
+})
+
+router.get("/theteam", (request, response) => {
+    response.send("This is the team route")
+})
+
+router.get("/signup", (request, response) => {
+    response.send("This is the signup route")
+})
+
+router.get("/login", (request, response) => {
+    response.send("This is the login route")
+})
+
+router.get("/logout", (request, response) => {
+    response.clearCookie("sessionJWT");
+    response.send("You have been successfully logged out");
+});
+
+module.exports = router;
