98ff43549096e520228b2acd79a367696097a92a
Nikhil Singh <nikhilsingh498@gmail.com>
2019-05-05 02:31:47 +0530

    model update for image upload

--- model/model.js
+++ model/model.js
@@ -1,5 +1,39 @@
 const mongoose = require("mongoose");
 
+const preUserProfile = new mongoose.Schema({
+    FIRSTNAME : {
+        type : String,
+        required : true,
+        lowercase : true
+    },
+    LASTNAME : {
+        type : String,
+        required : true,
+        lowercase : true
+    },
+    EMAIL : {
+        type : String,
+        required : true,
+        lowercase : true,
+        unique : true
+    },
+    USERNAME : {
+        type : String,
+        required : true,
+        lowercase : true,
+        unique : true
+    },
+    PASSWORD : {
+        type : String,
+        required : true
+    },
+    TOKEN : {
+        type : String,
+        required : true 
+    }
+}) 
+
+
 const userProfile = new mongoose.Schema({
     FIRSTNAME : {
         type : String,
@@ -26,9 +60,17 @@ const userProfile = new mongoose.Schema({
     PASSWORD : {
         type : String,
         required : true
+    },
+    PICTUREURL : {
+        type : String,
+        required : true 
+    },
+    PICTUREID : {
+        type : String,
+        required : true
     }
-}) 
+})
 
 
-
+module.exports.preUserProfile = mongoose.model("preUserProfile", preUserProfile);
 module.exports.userProfile = mongoose.model("userProfile", userProfile);
