fff66f5507089fd6bd351642c2d99a787c7551bf
Nikhil Singh <nikhilsingh498@gmail.com>
2019-05-06 03:53:35 +0530

    Models updated
    ** updated according to the aadhar verification requirement

--- model/model.js
+++ model/model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// conatins the data after the registration and before image upload 
 const preUserProfile = new mongoose.Schema({
     FIRSTNAME : {
         type : String,
@@ -37,7 +38,7 @@ const preUserProfile = new mongoose.Schema({
     }
 }) 
 
-
+// contains the registeration data after the validation 
 const userProfile = new mongoose.Schema({
     FIRSTNAME : {
         type : String,
@@ -79,6 +80,60 @@ const userProfile = new mongoose.Schema({
     }
 })
 
+// will contain aadhar details 
+// will use this to verify the input details 
+const adminAadhar = new mongoose.Schema({
+    FIRSTNAME : {
+        type : String,
+        required : true,
+        lowercase : true,
+        trim : true
+    },
+    LASTNAME : {
+        type : String,
+        required : true,
+        lowercase : true,
+        trim : true
+    },
+    ID : {
+        type : String,
+        required : true,
+        lowercase : true,
+        unique : true,
+        trim : true
+    },
+    MOBILE : {
+        type : Number,
+        required : true,
+        trim : true
+    }
+})
+
+
+// admin related data 
+const admin = mongoose.Schema({
+    NAME : {
+        type : String,
+        required : true,
+        lowercase : true,
+        trim : true
+    },
+    USERNAME : {
+        type : String,
+        required : true,
+        lowercase : true,
+        unique : true,
+        trim : true
+    },
+    PASSWORD : {
+        type : String,
+        required : true
+    }
+})
+
 
 module.exports.preUserProfile = mongoose.model("preUserProfile", preUserProfile);
 module.exports.userProfile = mongoose.model("userProfile", userProfile);
+module.exports.adminAadhar = mongoose.model("adminAadhar", adminAadhar);
+module.exports.admin = mongoose.model("admin", admin);
+
