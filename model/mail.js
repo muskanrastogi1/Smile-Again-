87fd284e2982fb66643270c98ca18fdec81f79ed
Nikhil Singh <nikhilsingh498@gmail.com>
2019-05-07 21:58:02 +0530

    Config for Email added

--- model/mail.js
+++ model/mail.js
@@ -1,30 +1,31 @@
 var nodemailer = require('nodemailer');
 
 
-module.exports.email = (destination) => {
+module.exports.email = (dest) => {
 
     let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: process.env.EMAIL,
-          pass: process.env.PASSCODE
-        }
-      });
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSCODE
+      }
+    });
       
-      var mailOptions = {
-        from: 'smileagain@gmail.com',
-        to: destination,
-        subject: 'Meetup Details - Smile Again',
-        text: '//Content for meetup//'
-      };
+    var mailOptions = {
+      from: 'smileagain@gmail.com',
+      to: dest,
+      subject: 'Meetup Details - Smile Again',
+      text: '//Content for meetup//'
+    };
       
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+
 } 
 
 
