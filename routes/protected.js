3e0636e542a2995fec82c4880f09cbcaf24807d2
Nikhil Singh <nikhilsingh498@gmail.com>
2019-05-07 21:58:25 +0530

    Protected route init

--- routes/protected.js
+++ routes/protected.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
-
+// aadhar import for admin 
 
 
 router.get("/private", (request, response) => {
     response.send("This is a private page")
+})
+
+router.post("/admin/saveData", (request, response) => {
+    if ( request.decode.username == "satinj" || request.decode.username == "nikhils" || request.decode.username == "muskanr"){
+        // process form aadhar database
+    }
 })
 
 module.exports = router;
